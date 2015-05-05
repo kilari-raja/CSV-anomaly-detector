d5af6017c2a5516dce7e3fff61782855a822b6f0
wm <rajkumar.s@imaginea.com>
2015-05-05 10:23:16 +0530

    pattern_special_character_website changed as per RFC 3986 internet regulations

--- AnomalyDetector.py
+++ AnomalyDetector.py
@@ -117,7 +117,7 @@ class ExecuteProgram(object):
 		patter_zipcode_four_digits = re.compile('^\d{4}$')
 		pattern_zipcode_two_hyphen = re.compile('^\d{5}--\d{4}$')
 		pattern_special_characters = re.compile("[!|$|\\\\|/|%|^|+|=|_|*|}|~|\[|\]|:|?|`|<|>|{]")
-		pattern_special_characters_website = re.compile("[!|$|\\\\|%|^|+|=|_|*|}|~|\[|\]|:|?|`|<|>|{]")
+		pattern_special_characters_website = re.compile("[\\\\|%|^|\||}|`|<|>|{]")
 		pattern_special_characters_phone = re.compile("[!|$|\\\\|/|%|^|=|_|*|}|~|\[|\]|:|?|`|<|>|{]")
 		pattern_open_parantheses =  re.compile("[()]")
 		pattern_close_parantheses = re.compile("[)]")
@@ -225,7 +225,7 @@ class ExecuteProgram(object):
 									global func_count
 									func_count += 1	 							
 									fp.write("***************************************************************************************\n")
-									fp.write("THIS ROW IS PRINTED BECAUSE A STRING (WITHOUT HYPHEN) IS PRESENT IN PLACE OF INTEGER IN COLUMN ") 
+									fp.write("THIS ROW IS PRINTED BECAUSE A STRING (WITHOUT HYPHEN) IS PRESENT IN COLUMN ") 
 									fp.write(mylist[a])
 									fp.write(" OF THE CSV FILE\n")
 									fp.write("***************************************************************************************\n")
