fc4232aad095fa07b4e5383a50f0499d6b7575cb
wm <rajkumar.s@imaginea.com>
2015-06-11 14:59:11 +0530

    Splitting big arrays in single line to multiple lines

--- Reader.py
+++ Reader.py
@@ -5,10 +5,11 @@ from csv import reader
 from collections import Counter
 from zipcodes import zipcodes
 app = compago.Application()
+
 two_letter_lowercase_string_not_state_code = string_with_integer_spaces = \
 email = website = string_with_space_no_integer = phone_no_with_alphabets = \
 website_without_www = state_code = pure_uppercase_string = phone_no_two_hyphens\
- = phone_no_with_parantheses = phone_no_without_hyphen_or_alphabets =  empty =\
+= phone_no_with_parantheses = phone_no_without_hyphen_or_alphabets =  empty =\
 string_with_integer_without_spaces = pure_integer = \
 two_letter_uppercase_string_not_state_code = zipcode_with_two_not_successive_hyphens\
 = string_without_integer_without_spaces = string_with_symbol_instead_of_at \
@@ -26,9 +27,31 @@ special_characters_print =email_dominant_column= phone_three_parts_two_hyphens\
 =phone_11_digits=phone_10_digits=row_count= else_count = func_count = \
 returnPrintFunction = local_count = count = counter = globvar = purestring\
 = pureStringWithSpaces = you = integer_with_at =  row_count =0
-states = [ "AK","Alaska","AL","Alabama","AR","Arkansas","AS","American Samoa","AZ","Arizona","CA","California","CO","Colorado","CT","Connecticut","DC","District of Columbia","DE","Delaware","FL","Florida","GA","Georgia","GU","Guam","HI","Hawaii","IA","Iowa","ID","Idaho","IL","Illinois","IN","Indiana","KS","Kansas","KY","Kentucky","LA","Louisiana","MA","Massachusetts","MD","Maryland","ME","Maine","MI","Michigan","MN","Minnesota","MO","Missouri","MS","Mississippi","MT","Montana","NC","North Carolina","ND","North Dakota","NE","Nebraska","NH","New Hampshire","NJ","New Jersey","NM","New Mexico","NV","Nevada","NY","New York","OH","Ohio","OK","Oklahoma","OR","Oregon","PA","Pennsylvania","PR","Puerto Rico","RI","Rhode Island","SC","South Carolina","SD","South Dakota","TN","Tennessee","TX","Texas","UT","Utah","VA","Virginia","VI","Virgin Islands","VT","Vermont","WA","Washington","WI","Wisconsin","WV","West Virginia","WY","Wyoming"]
-state_code_array = ["AK","AL","AR","AS","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","PR","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"]
-states_and_cities = ["Wyoming","Minnesota","California","Georgia","Kansas","Vermont","Indiana","Pennsylvania","Alabama","New York","Florida","Ohio","Texas","Maryland","Louisiana","Missouri","WY","MN","CA","GA","KS","VT","IN","PA","AL","NY","FL","OH","TX","MD","LA"]
+
+states = [ "AK","Alaska","AL","Alabama","AR","Arkansas","AS","American Samoa",
+"AZ","Arizona","CA","California","CO","Colorado","CT","Connecticut","DC",
+"District of Columbia","DE","Delaware","FL","Florida","GA","Georgia","GU",
+"Guam","HI","Hawaii","IA","Iowa","ID","Idaho","IL","Illinois","IN","Indiana"
+,"KS","Kansas","KY","Kentucky","LA","Louisiana","MA","Massachusetts","MD",
+"Maryland","ME","Maine","MI","Michigan","MN","Minnesota","MO","Missouri","MS",
+"Mississippi","MT","Montana","NC","North Carolina","ND","North Dakota","NE",
+"Nebraska","NH","New Hampshire","NJ","New Jersey","NM","New Mexico","NV",
+"Nevada","NY","New York","OH","Ohio","OK","Oklahoma","OR","Oregon","PA",
+"Pennsylvania","PR","Puerto Rico","RI","Rhode Island","SC","South Carolina",
+"SD","South Dakota","TN","Tennessee","TX","Texas","UT","Utah","VA","Virginia",
+"VI","Virgin Islands","VT","Vermont","WA","Washington","WI","Wisconsin","WV",
+"West Virginia","WY","Wyoming"]
+
+state_code_array = ["AK","AL","AR","AS","AZ","CA","CO","CT","DC","DE","FL","GA"
+,"GU","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO",
+"MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","PR",
+"RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"]
+
+states_and_cities = ["Wyoming","Minnesota","California","Georgia","Kansas",
+"Vermont","Indiana","Pennsylvania","Alabama","New York","Florida","Ohio",
+"Texas","Maryland","Louisiana","Missouri","WY","MN","CA","GA","KS","VT","IN",
+"PA","AL","NY","FL","OH","TX","MD","LA"]
+
 lower_states = []
 upper_states = []
 zipcode_array = []
